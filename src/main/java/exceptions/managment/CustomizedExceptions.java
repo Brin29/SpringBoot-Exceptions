270a893d48f2e7563941e62f040886b09bc45a34
Brin29 <breinerstivenparracortes50@gmail.com>
2024-09-24 14:26:30 -0500

    sa

---
+++ src/main/java/exceptions/managment/CustomizedExceptions.java
@@ -0,0 +1,9 @@
+package exceptions.managment;
+
+public class CustomizedExceptions {
+    // Excepciones personalizadas
+    // Checked exceptions
+    // Agregarle atributos adicionales a la clase y msa informacion
+    // Su objetivo es proveer informacion
+
+}
